e5f30d50cb315a18e60448996f41153aa09e0a16
swalker- <stephenleewalker@gmail.com>
2015-10-13 14:54:42 -0500

    Initial commit

---
+++ VideoStore/Customer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace VideoStore
+{
+    public class Customer
+    {
+        private readonly ArrayList _rentals = new ArrayList();
+        public string Name { get; }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public void AddRental(Rental rental)
+        {
+            _rentals.Add(rental);
+        }
+
+        public string Statement()
+        {
+            decimal totalAmount = 0;
+            var frequentRenterPoints = 0;
+            var result = "Rental Record for " + Name + "\n";
+            foreach (var rental in _rentals.Cast<Rental>())
+            {
+                decimal thisAmount = 0;
+                switch (rental.Movie.PriceCode)
+                {
+                    case Movie.Regular:
+                        thisAmount += 2;
+                        if (rental.DaysRented > 2)
+                            thisAmount += (rental.DaysRented - 2)*1.5m;
+                        break;
+                    case Movie.NewRelease:
+                        thisAmount += rental.DaysRented*3;
+                        break;
+                    case Movie.Childrens:
+                        thisAmount += 1.5m;
+                        if (rental.DaysRented > 3)
+                            thisAmount += (rental.DaysRented - 3)*1.5m;
+                        break;
+                    default:
+                        break;
+                }
+
+                // add frequent renter points
+                frequentRenterPoints++;
+                // add bonus for a two day new release rental
+                if ((rental.Movie.PriceCode == Movie.NewRelease) &&
+                    rental.DaysRented > 1)
+                    frequentRenterPoints++;
+
+                // show figures for this rental
+                result += $"\t{rental.Movie.Title}\t"+ $"{thisAmount:F1}\n";
+                totalAmount += thisAmount;
+            }
+
+            // add footer lines
+            result += $"Amount owed is "+ $"{totalAmount:F1}\n";
+            result += $"You earned {frequentRenterPoints} frequent renter points";
+            return result;
+        }
+    }
+}
---
+++ VideoStoreSpec/VideoStoreSpec.cs
@@ -0,0 +1,79 @@
+﻿using NBehave.Spec.NUnit;
+using NUnit.Framework;
+using Rhino.Mocks.Impl.Invocation.Actions;
+using VideoStore;
+
+namespace VideoStoreSpec
+{
+    [TestFixture]
+    public class VideoStoreSpec
+    {
+        private Customer _customer;
+        private readonly Movie _newRelease1 = new Movie("New Release 1", Movie.NewRelease);
+        private readonly Movie _newRelease2 = new Movie("New Release 2", Movie.NewRelease);
+        private readonly Movie _childrens = new Movie("Childrens", Movie.Childrens);
+        private readonly Movie _regular1 = new Movie("Regular 1", Movie.Regular);
+        private readonly Movie _regular2 = new Movie("Regular 2", Movie.Regular);
+        private readonly Movie _regular3 = new Movie("Regular 3", Movie.Regular);
+
+        [SetUp]
+        public void Init()
+        {
+            _customer = new Customer("Customer Name");
+        }
+
+        private void AssertAmountAndPointsForStatement(double expectedAmount, int expectedPoints)
+        {
+            var statement = _customer.Statement();
+            statement.ShouldContain($"Amount owed is {expectedAmount:F1}");
+            statement.ShouldContain($"You earned {expectedPoints} frequent renter points");
+        }
+
+        [Test]
+        public void TestSingleNewReleaseStatement()
+        {
+            _customer.AddRental(new Rental(_newRelease1, 3));
+            AssertAmountAndPointsForStatement(9.0, 2);
+        }
+
+        [Test]
+        public void TestDualNewReleaseStatement()
+        {
+            _customer.AddRental(new Rental(_newRelease1, 3));
+            _customer.AddRental(new Rental(_newRelease2, 3));
+            AssertAmountAndPointsForStatement(18.0, 4);
+        }
+
+        [Test]
+        public void TestSingleChildrensStatement()
+        {
+            _customer.AddRental(new Rental(_childrens, 3));
+            AssertAmountAndPointsForStatement(1.5, 1);
+        }
+
+        [Test]
+        public void TestMultipleRegularStatement()
+        {
+            _customer.AddRental(new Rental(_regular1, 1));
+            _customer.AddRental(new Rental(_regular2, 2));
+            _customer.AddRental(new Rental(_regular3, 3));
+            AssertAmountAndPointsForStatement(7.5, 3);
+        }
+
+        [Test]
+        public void TestRentalStatementFormat()
+        {
+            _customer.AddRental(new Rental(_regular1, 1));
+            _customer.AddRental(new Rental(_regular2, 2));
+            _customer.AddRental(new Rental(_regular3, 3));
+
+            const string expectedStatement = "Rental Record for Customer Name\n" +
+                                             "\tRegular 1\t2.0\n" +
+                                             "\tRegular 2\t2.0\n" +
+                                             "\tRegular 3\t3.5\n" +
+                                             "Amount owed is 7.5\n" +
+                                             "You earned 3 frequent renter points";
+            _customer.Statement().ShouldEqual(expectedStatement);
+        }
+    }
+}
